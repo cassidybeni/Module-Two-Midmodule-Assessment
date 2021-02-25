a8d1f41d84aa796a3404983df40e4cbede44456d
cassidybeni <cassidybeni@pursuit.org>
2021-02-25 14:58:16 -0500

    Pass all tests

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     let btn = document.querySelector('#roll-dice')
     let clicks = 0
     let dice = ['⚀','⚁','⚂','⚃','⚄','⚅']
-    let sum = 0
 
-    
     btn.addEventListener('click', () => {
         clicks++
 
@@ -17,31 +15,30 @@ document.addEventListener("DOMContentLoaded", () => {
             str += dice[Math.floor(Math.random() * dice.length)]
             dicePara.innerText = str  
             
-            if(str[i].includes(dice[0])){
-                sum++
-            } else if(str[i].includes(dice[1])){
-                sum += 2
-            } else if(str[i].includes(dice[2])){
-                sum += 3
-            } else if(str[i].includes(dice[3])){
-                sum += 4
-            } else if(str[i].includes(dice[4])){
-                sum += 5
-            } else if(str[i].includes(dice[5])){
-                sum += 6
-            }
         }
-        sumPara.innerText = `Sum = ${sum}`
-
-
+        
         if(clicks >= 1){
+            let sum = 0
+            for(let i = 0; i < str.length; i++){
+                if(str[i].includes(dice[0])){
+                    sum++
+                } else if(str[i].includes(dice[1])){
+                    sum += 2
+                } else if(str[i].includes(dice[2])){
+                    sum += 3
+                } else if(str[i].includes(dice[3])){
+                    sum += 4
+                } else if(str[i].includes(dice[4])){
+                    sum += 5
+                } else if(str[i].includes(dice[5])){
+                    sum += 6
+                }
+            }
             let ul = document.querySelector('#history-list')
             let li = document.createElement('li')
             li.innerText = `${dicePara.innerText} = ${sum}`
             ul.appendChild(li)
-
+            sumPara.innerText = `Sum = ${sum}`
         }
-
     })
-
 })
