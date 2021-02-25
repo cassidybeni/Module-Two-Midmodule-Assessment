b7af8a949ecb303da11857f62afd4380bae14969
cassidybeni <cassidybeni@pursuit.org>
2021-02-25 16:09:01 -0500

    First click doesn't add to history

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
     let btn = document.querySelector('#roll-dice')
-    let clicks = 0
     let dice = ['⚀','⚁','⚂','⚃','⚄','⚅']
 
     btn.addEventListener('click', () => {
-        clicks++
 
         let input = Number(document.querySelector('#number-of-dice').value)
         let dicePara = document.querySelector('#dice-para')
@@ -17,28 +15,26 @@ document.addEventListener("DOMContentLoaded", () => {
             
         }
         
-        if(clicks >= 1){
-            let sum = 0
-            for(let i = 0; i < str.length; i++){
-                if(str[i].includes(dice[0])){
-                    sum++
-                } else if(str[i].includes(dice[1])){
-                    sum += 2
-                } else if(str[i].includes(dice[2])){
-                    sum += 3
-                } else if(str[i].includes(dice[3])){
-                    sum += 4
-                } else if(str[i].includes(dice[4])){
-                    sum += 5
-                } else if(str[i].includes(dice[5])){
-                    sum += 6
-                }
+        let sum = 0
+        for(let i = 0; i < str.length; i++){
+            if(str[i].includes(dice[0])){
+                sum++
+            } else if(str[i].includes(dice[1])){
+                sum += 2
+            } else if(str[i].includes(dice[2])){
+                sum += 3
+            } else if(str[i].includes(dice[3])){
+                sum += 4
+            } else if(str[i].includes(dice[4])){
+                sum += 5
+            } else if(str[i].includes(dice[5])){
+                sum += 6
             }
-            let ul = document.querySelector('#history-list')
-            let li = document.createElement('li')
-            li.innerText = `${dicePara.innerText} = ${sum}`
-            ul.appendChild(li)
-            sumPara.innerText = `Sum = ${sum}`
         }
+        let ul = document.querySelector('#history-list')
+        let li = document.createElement('li')
+        li.innerText = `${dicePara.innerText} = ${sum}`
+        ul.appendChild(li)
+        sumPara.innerText = `Sum = ${sum}` 
     })
 })
